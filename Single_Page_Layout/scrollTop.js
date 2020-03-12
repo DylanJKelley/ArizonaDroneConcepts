6b604c2811c13dbaf14dc83c26efc5dcab793cb6
Dylan Kelley <dylanjkelley@outlook.com>
2020-03-12 09:53:48 -0700

    Updates
    Added transitions to the back to top button when displaying, added scroll effect for back to top, added logo to navbar, added content to services section on multi page layout.

--- Single_Page_Layout/scrollTop.js
+++ Single_Page_Layout/scrollTop.js
@@ -3,7 +3,7 @@ var topButton = document.getElementById("scrollTop");
 
 // When the page loads, hide the back to top button.
 window.onload = function () {
-    this.topButton.style.display = "none";
+    this.topButton.opacity = 0;
 }
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -13,14 +13,10 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        topButton.style.display = "block";
+        topButton.style.opacity = 100;
+        topButton.style.transition = ".2s";
     } else {
-        topButton.style.display = "none";
+        topButton.style.opacity = 0;
+        topButton.style.transition = ".2s";
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 }
